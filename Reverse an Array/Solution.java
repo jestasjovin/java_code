f1ef84980819fce772feb17348999f14ee3ddbca
Jovine jestas <jestasjovin@gmail.com>
2024-11-18 13:56:29 +0300

    reverse an array

---
+++ Reverse an Array/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    
+    public int[] reverseArray(int[] nums) {
+         int[] result = {};
+         return result;
+
+     }
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] testing = {7,8,9,11,12};  
+        int[] result = solution.reverseArray(testing);
+        
+        System.out.println(result);
+    }
+}
