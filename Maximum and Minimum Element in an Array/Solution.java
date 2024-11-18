087b64c8374f46b5d475134c59099665533c3da1
Jovine jestas <jestasjovin@gmail.com>
2024-11-18 13:56:09 +0300

    maximum and minimum in array

---
+++ Maximum and Minimum Element in an Array/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    
+    public int[] maxMinElement(int[] nums) {
+         int[] result = {};
+         return result;
+
+     }
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] testing = {7,8,9,11,12};  
+        int[] result = solution.maxMinElement(testing);
+        
+        System.out.println(result);
+    }
+}
